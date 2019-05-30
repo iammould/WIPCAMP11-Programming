d90e2c85c70f84f0e72d99779e1605b2f885a371
iammould <mould2545@gmail.com>
2019-05-30 11:37:31 +0700

    [ADD] New Project
    WIP CAMP Day #2 Computer Programming

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("โมล มัทฉะ \n");
+        
+        System.out.print("เหวิน มัทฉะ \n");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.print("ลิน มัทฉะ \n");
+        
+        System.out.print("มัทฉะ");
+        
+    }
+}
---
+++ WIP-XI/src/III/Output/_1_Println.java
@@ -0,0 +1,18 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Println {
+    public static void main(String[] args) {
+        String name = "โมล";
+        System.out.println("หวัดดีค่าา หนูชื่อ " + name);
+        
+        System.out.println("ถ้าเราเอา " + 1 + " ไปบวก " + 100 + " เราจะได้ " + 1+100);
+    }
+}
